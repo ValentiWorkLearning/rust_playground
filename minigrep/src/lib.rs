a5e9e4947a0c966469af93de2b17fa4eda4ab9dc
Valentyn Korniienko <kornienko-vr@rambler.ru>
2023-01-09 00:32:30 +0200

    [minigrep] completed minigrep sample

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,16 +1,22 @@
 use std::error::Error;
-use std::fs;
+use std::{fs, vec,env};
 
 pub struct Config{
     pub query:String,
-    pub file_path:String
+    pub file_path:String,
+    pub search_mode:SearchingMode
+}
+#[derive(Clone,Copy)]
+pub enum SearchingMode {
+    CaseSensitive,
+    CaseInsensitive
 }
 
 pub fn _parse_config(args: &[String]) -> Config {
     let query = &args[1];
     let file_path = &args[2];
 
-    Config{query:query.to_string(), file_path:file_path.to_string()}
+    Config{query:query.to_string(), file_path:file_path.to_string(), search_mode:SearchingMode::CaseSensitive}
 }
 
 impl Config{
@@ -24,7 +30,14 @@ impl Config{
         let query = &args[1];
         let file_path = &args[2];
 
-        Ok(Config{query:query.to_string(), file_path:file_path.to_string()})
+
+        let search_mode_env =
+        match env::var("IGNORE_CASE").is_ok(){
+            true=> SearchingMode::CaseInsensitive,
+            false=>SearchingMode::CaseSensitive
+        };
+
+        Ok(Config{query:query.to_string(), file_path:file_path.to_string(),search_mode: search_mode_env})
     }
 }
 
@@ -40,5 +53,79 @@ pub fn run(config: &Config)->Result<(),Box<dyn Error>>{
     let file_content = fs::read_to_string(&filepath)?;
     
     println!("File content is: {file_content}");
+
+    println!("==grep result begin==");
+
+    for line in search(&config.query, &file_content,config.search_mode){
+        println!("{line}")
+    }
+
+    println!("==grep result end==");
+
     Ok(())
+}
+
+
+
+pub fn search<'a>(query: &str, contents: &'a str, search_strategy: SearchingMode)-> Vec<&'a str>{
+    let mut results = Vec::new();
+
+    for line in contents.lines(){
+        match search_strategy {
+            SearchingMode::CaseInsensitive=>{
+                let lowercase_query = query.to_lowercase();
+                if line.to_lowercase().contains(&lowercase_query){
+                    results.push(line);
+                }
+            }
+            SearchingMode::CaseSensitive=>{
+                if line.contains(query){
+                    results.push(line);
+                }
+            }
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn one_result(){
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query,contents,SearchingMode::CaseSensitive));
+    }
+
+
+    #[test]
+    fn no_results(){
+        let query = "none-existing-string";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three";
+        let empty_vec:Vec<&str> = Vec::new();
+
+        assert_eq!(empty_vec, search(query, contents,SearchingMode::CaseSensitive));
+    }
+
+
+    #[test]
+    fn case_insensitive(){
+        let query = "RuSt";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three";
+
+        assert_eq!(vec!["Rust:"], search(query,contents,SearchingMode::CaseInsensitive));
+    }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,13 +6,16 @@ use minigrep::Config;
 fn main() {
     let cmd_args:Vec<String> = env::args().collect();
 
-    let config = Config::new(&cmd_args);
+    let config = Config::new(&cmd_args).unwrap_or_else(|err|{
+        eprintln!("Problem with parsing arguments: {err}");
+        process::exit(1);
+        }
+    );
 
-    
     dbg!(cmd_args.clone());
 
-    if let Err(error) = minigrep::run(&config.unwrap()){
-        println!("Parsing error occured: {error}");
+    if let Err(error) = minigrep::run(&config){
+        eprintln!("Parsing error occured: {error}");
         process::exit(1);
     }
 }
