6822f922a6c0096da5718f368c82fa27b43a6ab1
Valentyn Korniienko <kornienko-vr@rambler.ru>
2023-01-10 14:50:17 +0200

    [closures_iterators] added iterators sample

--- functional_features/src/lib.rs
+++ functional_features/src/lib.rs
@@ -1,5 +1,5 @@
 use core::num;
-use std::thread;
+use std::{thread, vec};
 use std::time::Duration;
 
 #[derive(Debug,PartialEq,Copy,Clone)]
@@ -71,8 +71,8 @@ fn sorting_sample(){
     #[derive(Debug)]
     struct Rectangle{
         width:u32,
-        height:u32,
-    };
+        height:u32
+    }
 
     let mut list = [Rectangle{width:10,height:10},Rectangle{width:3,height:1}, Rectangle{width:8,height:1}];
 
@@ -86,6 +86,40 @@ fn sorting_sample(){
     println!("{:#?}",list);
     println!("Num sort operations: {num_sort_operations}");
 }
+
+
+fn array_slicing_test(){
+    let a:[u8;4] = [1,2,3,4];
+    let len = a.iter().len();
+
+    println!("TEST");
+    let items = &a[0..2];
+    for item in items {
+        dbg!(item);
+    }
+    println!("TEST");
+    let items2 = &a[0..=2];
+    for item in items2 {
+        dbg!(item);
+    }
+}
+
+fn vec_iterators_demo(){
+    let v1 = vec![1,2,3];
+    let v1_iter = v1.iter();
+
+    for val in v1_iter{
+        println!("Got value:{}",val);
+    }
+}
+
+//sample implementation of Iterator:
+pub trait IteratorSample {
+    type Item;
+    fn next(&mut self)->Option<Self::Item>;
+}
+
+
 pub fn functional_features_demo(){
     let store = Invertory{
         shirts:vec![ShirtColor::Blue,ShirtColor::Red,ShirtColor::Blue]
@@ -118,4 +152,75 @@ pub fn functional_features_demo(){
     captured_and_mutated();
     move_ownership();
     sorting_sample();
+    array_slicing_test();
+
+
+    vec_iterators_demo();
+}
+
+
+#[derive(PartialEq,Debug)]
+struct Shoe{
+    size:u32,
+    style:String,
+}
+
+
+fn shoes_in_size(shoes:Vec<Shoe>,shoe_size:u32)->Vec<Shoe>{
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+    #[test]
+    fn iterator_sample(){
+        let vec = vec![1,2,3];
+        let mut vec_iter = vec.iter();
+        assert_eq!(vec_iter.next(),Some((&1)));
+        assert_eq!(vec_iter.next(),Some((&2)));
+        assert_eq!(vec_iter.next(),Some((&3)));
+        assert_eq!(vec_iter.next(),None);
+    }
+
+    #[test]
+    fn iterator_sum(){
+        let v1 = vec![1,2,3];
+        let v1_iter = v1.iter();
+
+        let total:i32 = v1_iter.sum();
+        assert_eq!(total,6);
+    }
+
+    #[test]
+    fn iterator_map(){
+        let v1 = vec![1,2,3];
+        let v1_iter = v1.iter();
+        let second_vec: Vec<_> = v1_iter.map(|x| x * 2 ).collect();
+        assert_eq!(second_vec,vec![2,4,6]);
+
+    }
+
+
+    #[test]
+    fn shoe_capture_close_demo(){
+        let shoes = vec![
+            Shoe{
+                size:10,
+                style:String::from("sneaker"),
+            },
+            Shoe{
+                size:13,
+                style:String::from("sandal"),
+            },
+            Shoe{
+                size:10,
+                style:String::from("boot")
+            }
+        ];
+
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(in_my_size,vec![Shoe{size:10,style:String::from("sneaker")}, Shoe{size:10,style:String::from("boot")}]);
+    }
 }
